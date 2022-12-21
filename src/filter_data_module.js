96704c028fe49ce9c41d039a3f627e511af65888
Brian Wong <brianw7218@gmail.com>
2022-12-20 22:01:31 -0800

    Create module and method that returns relevant weather data

---
+++ src/filter_data_module.js
@@ -0,0 +1,26 @@
+const filterData = (() => {
+  const getTemperature = (data) => data.main.temp;
+  const getTemperatureFahrenheit = (data) => {
+    const kelvin = getTemperature(data);
+    const fahrenheit = ((kelvin - 273.15) * 9) / 5 + 32;
+    return Math.round(fahrenheit * 100) / 100;
+  };
+  const getTemperatureCelsius = (data) => {
+    const kelvin = getTemperature(data);
+    const celsius = kelvin - 273.15;
+    return Math.round(celsius * 100) / 100;
+  };
+  const getWeatherMain = (data) => data.weather[0].main;
+  const getWeatherDescription = (data) => data.weather[0].description;
+  const getWeatherIconCode = (data) => data.weather[0].icon;
+  return {
+    getTemperature,
+    getTemperatureFahrenheit,
+    getTemperatureCelsius,
+    getWeatherMain,
+    getWeatherDescription,
+    getWeatherIconCode,
+  };
+})();
+
+export { filterData };
