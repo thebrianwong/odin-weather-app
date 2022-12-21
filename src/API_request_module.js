7629b71fc9cf2975c66536fce4edbd969f75fda3
Brian Wong <brianw7218@gmail.com>
2022-12-20 22:34:40 -0800

    Start to try to add a try/catch statement to the API module

--- src/API_request_module.js
+++ src/API_request_module.js
@@ -1,11 +1,24 @@
 const APIRequest = (() => {
   const APIKEY = "a0c9839361f301aa4ff8bb089dde2d79";
   const getCityWeather = async (city) => {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`
-    );
-    const parsedObject = await response.json();
-    return parsedObject;
+    // const response = await fetch(
+    //   `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`
+    // );
+    // const parsedObject = await response.json();
+    // return parsedObject;
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`
+      );
+      console.log(response);
+      const parsedObject = await response.json();
+      return parsedObject;
+    } catch (error) {
+      console.log(error.cod);
+      if (error.cod === "404") {
+        console.log(error.message);
+      }
+    }
   };
   return { getCityWeather };
 })();
