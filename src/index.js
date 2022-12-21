428cb4f5486baacdd44e71984ee34d873d568ec2
Brian Wong <brianw7218@gmail.com>
2022-12-20 22:01:53 -0800

    Add more testing code for new methods

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import { APIRequest } from "./API_request_module";
+import { filterData } from "./filter_data_module";
 
 APIRequest.getCityWeather("San Francisco")
   .then((response) => {
     console.log(response);
     console.log(response.weather[0].icon);
-    const { icon } = response.weather[0];
+    const icon = filterData.getWeatherIconCode(response);
     const img = document.createElement("img");
     img.src = `http://openweathermap.org/img/wn/${icon}.png`;
     const div = document.querySelector("div");
     div.appendChild(img);
+    console.log(filterData.getWeatherMain(response));
+    console.log(filterData.getWeatherDescription(response));
+    console.log(filterData.getTemperature(response));
+    console.log(filterData.getTemperatureFahrenheit(response));
+    console.log(filterData.getTemperatureCelsius(response));
   })
   .then((response) => {
     console.log(response);
