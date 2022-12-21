8d718eec9c3d297cbb285f1ff76477212fc62112
Brian Wong <brianw7218@gmail.com>
2022-12-20 22:27:51 -0800

    Add a try/catch statement and prevent submitting if the input is empty

--- src/user_input_module.js
+++ src/user_input_module.js
@@ -4,22 +4,27 @@ import { filterData } from "./filter_data_module";
 const userInput = (() => {
   const addSearchButtonListener = () => {
     const searchButton = document.querySelector(".search-button");
-    searchButton.addEventListener("click", () => {
+    searchButton.addEventListener("click", async () => {
       const city = document.querySelector("#city").value;
-      APIRequest.getCityWeather(city).then((response) => {
-        console.log(response);
-        console.log(response.weather[0].icon);
-        const icon = filterData.getWeatherIconCode(response);
-        const img = document.createElement("img");
-        img.src = `http://openweathermap.org/img/wn/${icon}.png`;
-        const div = document.querySelector("div");
-        div.appendChild(img);
-        console.log(filterData.getWeatherMain(response));
-        console.log(filterData.getWeatherDescription(response));
-        console.log(filterData.getTemperature(response));
-        console.log(filterData.getTemperatureFahrenheit(response));
-        console.log(filterData.getTemperatureCelsius(response));
-      });
+      if (city !== "") {
+        try {
+          const response = await APIRequest.getCityWeather(city);
+          console.log(response);
+          console.log(response.weather[0].icon);
+          const icon = filterData.getWeatherIconCode(response);
+          const img = document.createElement("img");
+          img.src = `http://openweathermap.org/img/wn/${icon}.png`;
+          const div = document.querySelector("div");
+          div.appendChild(img);
+          console.log(filterData.getWeatherMain(response));
+          console.log(filterData.getWeatherDescription(response));
+          console.log(filterData.getTemperature(response));
+          console.log(filterData.getTemperatureFahrenheit(response));
+          console.log(filterData.getTemperatureCelsius(response));
+        } catch (error) {
+          console.log(error);
+        }
+      }
     });
   };
   return { addSearchButtonListener };
